6ef4e358fed80989f7ec9ebe96d34e47b0e7f2d0
Alfredo Montesinos <hugeyeti@protonmail.com>
2021-11-23 15:37:35 -0600

    Car Model+Controller+DBFaker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+12.times do
+  cars = Car.new(
+    brand: Faker::Vehicle.make,
+    model: Faker::Vehicle.model,
+    price: rand(100..5000),
+    capacity: rand(1..7),
+    rating: rand(1..10)
+  )
+  cars.save!
+
+  puts "saved correctly"
+end
