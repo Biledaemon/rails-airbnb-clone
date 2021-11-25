667b2255368152d5a91b43475bc270a67e966947
quintanavicuna <fquintanavicuna@gmail.com>
2021-11-25 11:39:38 -0300

    adding geocode to cars

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -8,6 +8,13 @@ class CarsController < ApplicationController
 
   def show
     @booking = Booking.new
+
+    @markers = @cars.geocoded.map do |car|
+      {
+        lat: car.latitude,
+        lng: car.longitude
+      }
+    end
   end
 
   def new
--- app/models/car.rb
+++ app/models/car.rb
@@ -1,5 +1,7 @@
 class Car < ApplicationRecord
   belongs_to :user
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 
   validates :brand, presence: true
   validates :model, presence: true
